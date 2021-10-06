ded9afad198074ff2d7e3026ee420838d26a1522
eliaraguso <elia.raguso@gmail.com>
2021-10-06 16:44:20 +0200

    migliorato concettualmente il render a schermo del titolo e del titolo originale di Films e Serie TV usando v-if e v-else

--- src/components/Film.vue
+++ src/components/Film.vue
@@ -1,7 +1,12 @@
 <template>
   <ul>
-    <li>Titolo: {{ film.title }}{{film.name}}</li>
-    <li>Titolo originale: {{ film.original_title }}{{film.original_name}}</li>
+    <li v-if="film.title">Titolo: {{ film.title }}</li>
+    <li v-else>Titolo: {{film.name}}</li>
+
+    <li v-if="film.original_title">Titolo originale: {{ film.original_title }}</li>
+    <li v-else>Titolo originale: {{film.original_name}}</li>
+
+
     <li>Lingua:<country-flag :country="(film.original_language == 'en') ? 'us' : film.original_language"/></li>
     <li>Voto: {{ film.vote_average }}</li>
     <li><img :src="'https://image.tmdb.org/t/p/w300' + film.poster_path" alt=""></li>
--- src/components/Films.vue
+++ src/components/Films.vue
@@ -1,6 +1,11 @@
 <template>
   <main>
+    <!-- Ciclo per i film -->
+    <h2>Films</h2>
     <Film v-for="film in arrayFilms" :key="film.id" :film="film" />
+
+    <!-- Ciclo per le serie TV -->
+    <h2>Serie TV</h2>
     <Film v-for="film in arraySeries" :key="film.id" :film="film" />
 
   </main>
